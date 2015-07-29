4c5bff99d4d2caf6944ab4022fe97968c9a1529b
Yicheng Wang <wyc25013@gmail.com>
2015-07-28 23:32:49 -0400

    ad_dbfile

---
+++ androidAppServer/dbForApp.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 29, 2015 at 05:30 AM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `dbForApp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userInfo`
+--
+
+CREATE TABLE IF NOT EXISTS `userInfo` (
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `first name` varchar(20) NOT NULL,
+  `last name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='db for storing user information';
+
+--
+-- Dumping data for table `userInfo`
+--
+
+INSERT INTO `userInfo` (`email`, `password`, `first name`, `last name`) VALUES
+('yichwang@umich.edu', 'Fendou2013', 'Yicheng', 'Wang');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userInfo`
+--
+ALTER TABLE `userInfo`
+  ADD PRIMARY KEY (`email`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
