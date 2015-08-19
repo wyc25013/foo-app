5de31c9803baa0ac9bce570fe4ca25011c7e31ae
Yicheng Wang <wyc25013@gmail.com>
2015-08-19 17:06:02 -0400

    mysql case sensitve issue fixed

--- androidAppServer/patientLogin.php
+++ androidAppServer/patientLogin.php
@@ -5,7 +5,7 @@ require 'connectdb.inc.php';
 $account = $_POST['postAcct'];
 $password = $_POST['postPwd'];
 
-$query = "SELECT email, password FROM userInfo WHERE email = '$account' AND password = '$password';";
+$query = "SELECT email, password FROM userInfo WHERE BINARY email = '$account' AND BINARY password = '$password';";
 if($query_run = mysql_query($query)){
 	$query_num_rows = mysql_num_rows($query_run);
 	if($query_num_rows == 1){
